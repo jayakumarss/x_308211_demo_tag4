4c36fff887ea09255dab1fc246ae796654db6679
System Administrator <admin@example.com>
2019-11-25 13:53:41 -0500

    Changes by 'System Administrator' on 'Demo TAG4 - 1.0.0' -- Build #1

---
+++ sn/Demo TAG4/Dictionary Entry/x_308211_demo_tag4_table/active_true/collection/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : Demo TAG4
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-11-25 18:52:22
+ * Created By  : admin
+ * Updated On  : 2019-11-25 18:52:22
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=7a224d92db150010343f58b3ca9619c6
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
