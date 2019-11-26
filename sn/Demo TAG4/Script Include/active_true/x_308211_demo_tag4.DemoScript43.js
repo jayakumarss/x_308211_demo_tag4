619848da44a17c3c2b65a520d87ca30f45581003
System Administrator <admin@example.com>
2019-11-26 08:07:16 -0500

    Changes by 'System Administrator' on 'Demo TAG4 - 1.0.4' -- Build #1

---
+++ sn/Demo TAG4/Script Include/active_true/x_308211_demo_tag4.DemoScript43.js
@@ -0,0 +1,28 @@
+/* 
+ * Application : Demo TAG4
+ * ClassName   : sys_script_include
+ * Created On  : 2019-11-26 13:06:45
+ * Created By  : admin
+ * Updated On  : 2019-11-26 13:06:50
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=9d9cb4a6db9d0010343f58b3ca961950
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_308211_demo_tag4.module:sys_script_include
+ */
+var DemoScript43 = Class.create();
+DemoScript43.prototype = /** @lends x_308211_demo_tag4.module:sys_script_include.DemoScript43.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+    },
+
+    type: 'DemoScript43'
+};
