855aaac8388328fe7d5089820227a8fb8961567f
System Administrator <admin@example.com>
2019-12-06 10:34:26 -0500

    Changes by 'System Administrator' on 'Demo TAG4 - 1.0.15' -- Build #1

---
+++ sn/Demo TAG4/Client Script/{--none--}/active_true/DemoTAG14.js
@@ -0,0 +1,14 @@
+/* 
+ * Application : Demo TAG4
+ * ClassName   : sys_script_client
+ * Created On  : 2019-12-06 15:27:43
+ * Created By  : admin
+ * Updated On  : 2019-12-06 15:27:43
+ * Updated By  : admin
+ * URL         : /sys_script_client.do?sys_id=137c747ddbad0010343f58b3ca961920
+ */
+test();
+function test1(){
+	var test1;
+	alert('test');
+}
