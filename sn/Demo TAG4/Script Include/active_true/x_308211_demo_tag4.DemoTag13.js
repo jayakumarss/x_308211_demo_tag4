a9c9863ea3a596f1e5b1bede0e62c26003879103
System Administrator <admin@example.com>
2019-12-10 09:37:00 -0500

    Changes by 'System Administrator' on 'Demo TAG4 - 1.0.16' -- Build #1

---
+++ sn/Demo TAG4/Script Include/active_true/x_308211_demo_tag4.DemoTag13.js
@@ -0,0 +1,33 @@
+/* 
+ * Application : Demo TAG4
+ * ClassName   : sys_script_include
+ * Created On  : 2019-12-06 15:23:19
+ * Created By  : admin
+ * Updated On  : 2019-12-10 14:34:07
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=3c9b783ddbad0010343f58b3ca961906
+ */
+/**
+ * Class Description
+ * 
+ * @class 
+ * @author System Administrator [admin]
+ * @memberof x_308211_demo_tag4.module:sys_script_include
+ */
+var DemoTag13 = Class.create();
+DemoTag13.prototype = /** @lends x_308211_demo_tag4.module:sys_script_include.DemoTag13.prototype */ {
+    /**
+     * Description
+     * 
+     * @returns {undefined}
+     */
+    initialize: function() {
+		//testCodeError();
+    },
+		dummy: function(){
+		gs.addInfoMessage("Testing devops");
+		return true;
+	},
+
+    type: 'DemoTag13'
+};
